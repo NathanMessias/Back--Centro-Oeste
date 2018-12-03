84520a95a35143731ee0137475fe7e10e34d3921
xj6 <nathan.10.mf@gmail.com>
2018-12-03 20:37:23 -0200

    Centro Oeste
    Projeto Millys

---
+++ Web-Back-master/src/main/java/com/unievangelica/progwebback/dominio/produto/ProdutoRepositorio.java
@@ -0,0 +1,15 @@
+package com.unievangelica.progwebback.dominio.produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
+
+    public List<Produto> findByCategoriaId(Long categoriaID);
+
+
+
+}
